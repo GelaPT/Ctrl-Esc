c0302a7a0d9f7d9b1db7f723b8eb858b325c8c66
GelaPT <andremgmonteiro@gmail.com>
2021-05-08 18:30:36 +0000

    Reorganização | Começo do trabalho em UIManager

--- Assets/Player/PlayerScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1eedf3ceab7f18dd6ef1176c14aed783b362552ae4e267ae7d1e1aad466b9b1d
-size 2740
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c01ebd3a4c57edcebc1a7ef924ceb19b90f713cb542bd60a08016e104e9dd9da
-size 1353
+oid sha256:417c95319937c6653ef8325ea05b08455e1973b2260f325ba9d6bf48ef7d4b9c
+size 2128
