71254c571ead0d5c44b41a5e7ad90b70c49a690d
Lucas <lucas_cmattos@hotmail.com.br>
2021-05-16 15:06:04 +0000

    porta, player e post processing

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f880a45096a58d79763ae68d553a7b996074d2a5330c66dbe841f8afa4e6cad0
-size 3243
+oid sha256:42a91f8c3fa6efdacaa933e4c863b8e3f3b5efae2f999a18ccbd060f1bd959ea
+size 3223
