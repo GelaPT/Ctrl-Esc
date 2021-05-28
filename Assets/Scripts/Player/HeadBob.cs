3355e3c977b1b2f01da2ef0d776b6f3568e2e4de
Lucas <lucas_cmattos@hotmail.com.br>
2021-05-28 10:33:14 +0000

    teste de player script 2

---
+++ Assets/Scripts/Player/HeadBob.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b4481e5be2a84c07a62ccb621bd3c9e691e364778546dabbac8604bd1766f20
+size 1107
---
+++ Assets/Scripts/Player/PlayerScriptDois.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb448c4054f644946f6347313bf7fe08adc959c4f4af38d41b26d59a5fdfaac3
+size 2290
