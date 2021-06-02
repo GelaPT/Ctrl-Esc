666f9e9868f11a0309be1614bc4318a0f055586f
HawlNix <ruicorreia200027@gmail.com>
2021-06-02 15:56:19 +0000

    No Comments

--- Assets/Scripts/Player/InteractScript.cs
+++ Assets/Scripts/Player/InteractScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04069f45dffe9dee7e256640cc3a3f1e886a25d3bad287d0c47a0f4e88ae73ec
-size 1130
+oid sha256:5f0a5a3c187e9c69c3ad26c61fac200814d8a0255b69ac633cbd95be923e0da6
+size 1188
