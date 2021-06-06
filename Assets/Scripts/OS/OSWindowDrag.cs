ed6d1224e010328278e02d1a1a590658f71233d8
GelaPT <andremgmonteiro01@gmail.com>
2021-06-06 01:52:38 +0000

    No Comments

---
+++ Assets/Scripts/OS/OSWindowDrag.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b54d3e83782d5b506d9179481815560d2d9161f30339f6ea977f6b412bc1b86b
+size 619
--- Assets/Scripts/Tasks/TaskManager.cs
+++ Assets/Scripts/Tasks/TaskManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8bf827115bab75b09a16d40d0d2eb778572de225046b52b899a5d54b69905f7
-size 1091
+oid sha256:0987435e38f716f0e090bad84438a94f288a0b3f231feb877ac53a2a840f0daa
+size 1086
