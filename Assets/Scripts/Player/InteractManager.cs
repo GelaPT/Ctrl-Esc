12540e06038ead3e614d6543a928f6b4b2c2d328
GelaPT <andremgmonteiro@gmail.com>
2021-06-07 01:55:02 +0000

    No Comments

--- Assets/Scripts/Player/InteractManager.cs
+++ Assets/Scripts/Player/InteractManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6d6fd9ea8672fa65b267bd6d44a2043e8b563a4908ea82eb61e9cae88c3cf34
-size 1118
+oid sha256:90a1a7f58a23fbbe64af926cf1adaca423176fa9b310e90ed015f525ead8b699
+size 1158
