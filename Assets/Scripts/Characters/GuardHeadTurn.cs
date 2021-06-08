644927e95250bcea0e786bbd776130d0f9c1e3c4
HawlNix <ruicorreia200027@gmail.com>
2021-06-08 16:49:27 +0000

    No Comments

---
+++ Assets/Scripts/Characters/GuardHeadTurn.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0e696310e18d42413aa22ada65c1a86d787fb6780613e4ff2586e07a6975f5b
+size 322
---
+++ Assets/Scripts/Characters/GuardScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f90ecb1cd40a2e85e41cf427314470614fad1aec294b774ba1244ca88f2b8a7
+size 1068
