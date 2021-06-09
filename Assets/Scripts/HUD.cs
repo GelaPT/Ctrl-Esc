300856a270985edf368c2fd45b414d2c75490ca5
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-09 12:26:53 +0000

    No Comments

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:065443ad11d817ab4a4a518f76f975783eeb18d16b44ad6cbb63e58ccf114b53
-size 578
+oid sha256:2e489f56a37d97af99810c94879784c6d61a63e1a6e5240b8a26534171ac598a
+size 1138
