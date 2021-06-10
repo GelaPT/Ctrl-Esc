45c51ce394fa58d7ab0b84285770beb02d301fe4
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-10 19:31:45 +0000

    No Comments

--- Assets/Scripts/Characters/GuardHeadTurn.cs
+++ Assets/Scripts/Characters/GuardHeadTurn.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1ce88979853a64ca01d1911b9c80d62900754d041f4c81be167a98abf10df5b
-size 760
+oid sha256:9aa8d2b682655d362c379a7ad5e4200115f4d3dccad7609a0d3b0f3a28d389b2
+size 706
