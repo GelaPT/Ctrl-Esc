c72d493f230365fa55612d2d68ce8909ffada8f8
GelaPT <andremgmonteiro01@gmail.com>
2021-06-10 17:39:00 +0000

    No Comments

---
+++ Assets/Scripts/OS/OSPrograms/OSCalcwaiter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63e0c2d8d4fc9401f4b44c05aa547ca6a72c688682c09fe6cfb83602599f0066
+size 1707
--- Assets/Scripts/OS/OSWindowDrag.cs
+++ Assets/Scripts/OS/OSWindowDrag.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8b92bcf4e4668719063a8e762f5ff1435b54c5d47c53ddb659c10e1a8e4d672
-size 1044
+oid sha256:dbfe598bf5a09fe270d08e8b4ebbc5457df7f4986d2e5dd1a10c12846ba7b09b
+size 1069
