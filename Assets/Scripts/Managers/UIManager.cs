95c4a5dff5f5b4bf8850cdbfd82bcde48ab96cd3
GelaPT <andremgmonteiro@gmail.com>
2021-06-10 21:32:18 +0000

    No Comments

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2b1566ded2b712d691fd7ab7422e029436f288319d9d7bde2bd740db0fe24b4
-size 1670
+oid sha256:cd7403da6edb5373903a8d8d6f8b8aec9cb8f3ca2119364cf5f6930ed9016fa5
+size 1651
--- Assets/Scripts/OS/OSWindowDrag.cs
+++ Assets/Scripts/OS/OSWindowDrag.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbfe598bf5a09fe270d08e8b4ebbc5457df7f4986d2e5dd1a10c12846ba7b09b
-size 1069
+oid sha256:9865d8dd7198a57cb85d730f34a4d0c1182c81e2708fcf475bebe913504a1e50
+size 1127
