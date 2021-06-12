34a1c8dbe36816ba1024b0929787e822f4b8b122
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-12 13:01:21 +0000

    No Comments

--- Assets/Scripts/Props/SecurityCameraScript.cs
+++ Assets/Scripts/Props/SecurityCameraScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ffe717676e2ec6451edb4b7c470a57dfb8c5febbda7e039770e7a5ce3ccc6c2
-size 2227
+oid sha256:5df399e7536ea354b3e4a61ac7785055083a9c39fed1cb3f36846db5af6d4045
+size 2153
