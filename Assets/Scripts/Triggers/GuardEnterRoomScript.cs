af403de8f2e7ad418ab676640892130be2c8b93d
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-16 12:13:57 +0000

    No Comments

--- Assets/Scripts/Triggers/GuardEnterRoomScript.cs
+++ Assets/Scripts/Triggers/GuardEnterRoomScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:894d0b5a492998b60eb8cc8f943aabc71460a680aa963b49267ed432b5ef79b3
-size 460
+oid sha256:03eeb32e2fa48da22b031d9346af52299556f3f1f884c6c2605f68b767f66fad
+size 490
---
+++ Assets/Scripts/Triggers/GuardGoToNextPosition.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5dd211cbdbe64e5c6c3411c0f02a51b77a04b34cc4cf81194d3a525282a8c77
+size 447
