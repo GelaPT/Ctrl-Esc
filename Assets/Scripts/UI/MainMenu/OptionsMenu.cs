048bce4c08e6842bd6288345f1673dd41f7d9c53
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-16 18:19:01 +0000

    brightness

--- Assets/Scripts/UI/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/UI/MainMenu/OptionsMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4700666834a52ffaa4750161ba04ed61e550c6def03e7e351dec85b29270a5f6
-size 1762
+oid sha256:141ca7c16cea27da7bd1549b9b5d518dd65dd1897f7220aebc944f3f798b81d9
+size 2237
