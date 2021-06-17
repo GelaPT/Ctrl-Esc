a90e61b917cb3fbca85e919077b9896f32d94545
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-17 22:15:10 +0000

    No Comments

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6f23c1e8c379bcfa419b1f513384c254ea3c5a1a4e77c88b5950f3cc42fa0c
-size 2513
+oid sha256:d71cf2741b68d626fcbb79e94292266052f927484cb072cd7dd3ae42bf1cdd44
+size 2749
