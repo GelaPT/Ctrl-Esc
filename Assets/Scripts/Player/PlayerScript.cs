9ba7aa85744c839ec3d2589a6c39e2773a2c257e
GelaPT <andremgmonteiro@gmail.com>
2021-06-17 16:49:57 +0000

    No Comments

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3f42e54179b82e2a276e1accc0b685871754aa0f5df6785a375426c9fd70bb6
-size 2514
+oid sha256:ef6f23c1e8c379bcfa419b1f513384c254ea3c5a1a4e77c88b5950f3cc42fa0c
+size 2513
