dd189342706853fb4cea1535efbcd226ae6b9b42
GelaPT <andremgmonteiro@gmail.com>
2021-06-17 16:01:03 +0000

    No Comments

--- Assets/Scripts/UI/MainMenu/MainMenuButtonManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuButtonManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2172956f21def9e201ed9cbca55939a66b4b0b691f658ccc63d65b9b818d5bc3
-size 2488
+oid sha256:03b589a78b1263e6d43babab2559054f2c1c3edf49971f20f4d992d95d27f594
+size 2886
