ef3ed84ef88f56ca6ec2b9f0dc6a8c91cc772248
GelaPT <andremgmonteiro01@gmail.com>
2021-06-17 17:07:58 +0000

    No Comments

--- Assets/Scripts/Tasks/FuseboxTask.cs
+++ Assets/Scripts/Tasks/FuseboxTask.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32b739466091ec732bba0054bf963bd1645b354d08f6779071fbb7d6ac50ced1
-size 304
+oid sha256:a62722316c5d335214883aced32accde0d43f1f4ecbd97824c384b8d2f6dfed7
+size 903
--- Assets/Scripts/Tasks/TaskManager.cs
+++ Assets/Scripts/Tasks/TaskManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3b7872fc4b08330f97fcdce6419bd6aee7f8ab34d390352ba297382b3e57f15
-size 1344
+oid sha256:cb82d5ce7713ca3b568f4e97941070f49202bba9ea62c07b9f114862e454b99f
+size 1172
