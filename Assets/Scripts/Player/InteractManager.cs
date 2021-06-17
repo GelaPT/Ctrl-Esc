e05dbfef600a0f67753ed920e751c1bc35afc35e
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-17 14:23:07 +0000

    lanterna e fuse box

--- Assets/Scripts/Player/InteractManager.cs
+++ Assets/Scripts/Player/InteractManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25a148046dfd91d0057c547a34e13caa9be7316dbcbdd6e3c1425e2814467cd0
-size 1928
+oid sha256:9c7929eb9756766197b2ba398ccae6ed8f0df0909e155a2879ef1c90682cc6d4
+size 2145
