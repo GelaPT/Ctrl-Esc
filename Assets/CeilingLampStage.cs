68d5d417a65fdbaf5684dd95419ede9b258eb44a
GelaPT <andremgmonteiro01@gmail.com>
2021-06-18 22:46:42 +0000

    No Comments

--- Assets/CeilingLampStage.cs
+++ Assets/CeilingLampStage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1411f660d063e5bf2531d3e16e0698a2cdae166007d8b554a0e3808ca78430c6
-size 672
+oid sha256:fb2777c56cea0bcccc457953cb4bcdfcbaa4d3fbc1e8ffa1b4cef3d79dc9055b
+size 656
--- Assets/Scripts/Props/ProjectorScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b2fb6dcfba92a9ddf35504d111a1c1695858e321fda269c6cf30b8a8d2c654f3
-size 3605
