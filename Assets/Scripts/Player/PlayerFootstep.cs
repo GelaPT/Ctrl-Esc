46677646a593d0e91d69816f2bb495b117115891
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-18 12:21:09 +0000

    No Comments

---
+++ Assets/Scripts/Player/PlayerFootstep.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:249b30ae3c42b4df1bc8295aea664ac4d857502952ae127d2878e5b6058ca146
+size 137
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e6cca9b3d428f4e16e251a11cdd2ce239b387bb018e7ce11f3f339d1b122354
-size 3116
+oid sha256:21ad73f2cf59fdaa590586a3d3034b1bdb9798dcc13eaaed6c02036fd58c6378
+size 2964
