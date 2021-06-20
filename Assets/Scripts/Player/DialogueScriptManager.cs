653ee50a19f889bfff038f3b6407e55dd0216d87
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-20 16:47:44 +0000

    No Comments

--- Assets/Scripts/Player/DialogueScriptManager.cs
+++ Assets/Scripts/Player/DialogueScriptManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55f6a5ce5855dabaf4af0212b574f593bbf1ae2eb6b34d5e66aa114859141882
-size 280
+oid sha256:b3150c68bc35afc3a77a7d8a9c1ce61ae859cc9f820027bd4746f021c3b56d3c
+size 320
