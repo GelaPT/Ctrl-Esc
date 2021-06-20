11e160130b93e9abc42a7c5d8b4eb64cbbab23ec
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-20 16:47:44 +0000

    No Comments

--- Assets/Scripts/Characters/GuardDialogueScript.cs
+++ Assets/Scripts/Characters/GuardDialogueScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d5322e711a2b39b0a9f4366ac558128cf53d4b4ec60d0443f05a7907a4cfcd
-size 3539
+oid sha256:dd7fcdbcfd45cad3364d4497af1d1870485b44f4712598a21a48d4c2a14b2e13
+size 4056
