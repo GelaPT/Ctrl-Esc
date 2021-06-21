cda0fc806c1271d241ff85ab67147584ba3742fb
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-21 20:41:50 +0000

    ai feita

--- Assets/Scripts/Characters/DeadMannequinScript.cs
+++ Assets/Scripts/Characters/DeadMannequinScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a26e1e3860fdf9e908cefac557c11a3ad3efc40e742d898fee6a5c481ae3035
-size 3582
+oid sha256:d486ccde3a1cb48f2db8f29e4c703e93f2f7391b17827dbc33da1b34e26558e8
+size 3482
