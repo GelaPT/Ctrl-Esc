2084317bba392b551d0e59f79501b1593311b0c8
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-21 20:41:50 +0000

    ai feita

--- Assets/Scripts/Characters/RoombaScript.cs
+++ Assets/Scripts/Characters/RoombaScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26770ac235954004cd31c612c03b29b57011ee3dbf37538cafdc030b48d0646a
-size 4546
+oid sha256:11af2ddc82c9715bc2092187e582d5d963ecbd032e488524330ec23159943cdf
+size 5210
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c32fd9418b7ca702330deaae2d50b927ec8e95fc39196d8bb203af2ba4cb7299
-size 2765
+oid sha256:a370c8a31f2b61cee532cbbfa5be525b78deba4f7e2fa1dee55a7e59309e84fa
+size 2946
