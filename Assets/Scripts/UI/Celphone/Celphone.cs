3e4c759a286a2c905d70b0a5f3d5598a7e77feae
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-21 11:28:40 +0000

    No Comments

--- Assets/Scripts/UI/Celphone/Celphone.cs
+++ Assets/Scripts/UI/Celphone/Celphone.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3918dc46ca3555b385b7bae214a9f65666110079a84441d9d6fbb159de3eed1c
-size 3637
+oid sha256:73f335725a6eef486ba24ce63fe99469017d2d5cf385a31b3c5ef62e59ef8994
+size 3728
