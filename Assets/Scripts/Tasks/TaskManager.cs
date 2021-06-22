7d00ae5d8794e37a2fa7728be7ce8f416975b8d1
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-22 23:14:57 +0000

    No Comments

--- Assets/Scripts/Tasks/TaskManager.cs
+++ Assets/Scripts/Tasks/TaskManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16eec651af668b963dab6ba5a82aabc31c0249f81f6327e467544e66a8645088
-size 6247
+oid sha256:e52e6e889a3aeeec9fae8a5881fd250e5bc3276f3c133bb2f0d01935814632a9
+size 6369
