909757a5daf1d75aa0e62fa45137923690d28a8b
GelaPT <andremgmonteiro@gmail.com>
2021-06-22 17:28:16 +0000

    No Comments

---
+++ Assets/Scripts/Editor/Shortcuts.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:045eef427e9e129cdb74d701091f57229a661cd7c36c7ba0e39f417fcc52fae3
+size 314
---
+++ Assets/Scripts/Save/OptionsData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:93bd89e785a90f116917a868c62eaff35befca189a0121c69ff14ae098c0c006
+size 720
