22593c77966611c10c79892c3e0196a69e8a1fc1
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-22 23:29:56 +0000

    No Comments

--- Assets/Scripts/UI/HintsScript.cs
+++ Assets/Scripts/UI/HintsScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1e7568e1ace0725990e007361fb7e9858c6e0b4886d3f833d9e3b4b75f95aa9
-size 546
+oid sha256:12dc76da7637eeb1d05883e4cab3b5c7069e8328089b77a8ce9a3a62c30c3db3
+size 705
