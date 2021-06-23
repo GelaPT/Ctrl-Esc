ee20a281f22e715b00076f4103a048862a8329fe
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 17:02:54 +0000

    No Comments

--- Assets/Scripts/Managers/MonologueManager.cs
+++ Assets/Scripts/Managers/MonologueManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8e33817c1f79df86237a4bf5b4a3ee1bbf683a9407f683892a064c26a3ca39b
-size 1845
+oid sha256:04bc7900915abb7d964bd4925b9d19809bef897e8bce0ac821e82fb433ae06d2
+size 1792
--- Assets/Scripts/Tasks/FuseboxTask.cs
+++ Assets/Scripts/Tasks/FuseboxTask.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22caaaf31af4312cc74d60aa444099e586c98dfc2b4e6c1ef8a51a9000c88f78
-size 6821
+oid sha256:425a4d30b316a91e3fa61bbf31e8d363bb29d69f1a5be98f712ed68bd68ab644
+size 6825
