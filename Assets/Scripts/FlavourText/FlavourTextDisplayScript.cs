df33c6f10a55c9301e2a76f0748fcde46bb62de3
GelaPT <andremgmonteiro01@gmail.com>
2021-06-23 15:46:43 +0000

    No Comments

--- Assets/Scripts/FlavourText/FlavourTextDisplayScript.cs
+++ Assets/Scripts/FlavourText/FlavourTextDisplayScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab1160dd8d599bc79a4555342a07bfee92fe879dd6ca3e32081de08c2ab168c1
-size 1535
+oid sha256:884e5715cdd41833fc7566faa372a3f7480acdab99cdac529468ab0d33305c83
+size 1502
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb229484c6a969f9787fa0d3e4b73150c391691bbf037a5f5c6ee14261a8b830
-size 5134
+oid sha256:615dfb9d2534452b8c68cc2254e1fc9b2c640e72a7641def24a3b74c64b90b66
+size 4968
