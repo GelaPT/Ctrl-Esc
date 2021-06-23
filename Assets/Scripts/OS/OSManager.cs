a19745b6293366561503805115aaf8ab9081c55f
GelaPT <andremgmonteiro@gmail.com>
2021-06-23 23:50:29 +0000

    No Comments

--- Assets/Scripts/OS/OSManager.cs
+++ Assets/Scripts/OS/OSManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b58657ebc318950e2d9c5576f56423633d2dcc199dd792bc9bc8b3ec53cc4dd0
-size 11066
+oid sha256:422dc8176494052d2c0c2730fcfde6f58be9c61378adb15ba96207f9ee09b492
+size 11027
