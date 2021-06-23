88273d0f0e756731481c643787eb9df00c647cd2
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 17:43:03 +0000

    No Comments

--- Assets/Scripts/Tasks/CollectFuse.cs
+++ Assets/Scripts/Tasks/CollectFuse.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bd43b814b3c663f531bc67797c1fe8c6a54a8f921f2e02edf2886d41d3b3395
-size 244
+oid sha256:13999ec96417d6f84380cbbbbf45542b70e7c627a0fc8e2df547988bf74007bf
+size 339
--- Assets/Scripts/Tasks/FuseboxTask.cs
+++ Assets/Scripts/Tasks/FuseboxTask.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76fe38a33bc6441bdbd37cf84c7b2476565ed4ca9057f62b06e783c7ad95b82a
-size 7440
+oid sha256:1399c6ce86785ee4c9d67f5a44df75b880c489f0dd805ecb18dd57249c5d67c7
+size 7589
