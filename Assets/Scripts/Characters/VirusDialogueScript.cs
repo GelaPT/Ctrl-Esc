0d572873e515ad171c6a43eb73117db50ee78f80
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 21:29:31 +0000

    No Comments

---
+++ Assets/Scripts/Characters/VirusDialogueScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9548e4201fe38fad80c6ab765c6344090e928eec18438e67516884a7f1a9cc15
+size 2652
--- Assets/Scripts/Player/InteractManager.cs
+++ Assets/Scripts/Player/InteractManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edd195ae2a1000d0e6e32b1d5d12424416ac59cea32fe83e99043b9cda49127f
-size 2387
+oid sha256:6d1780e7b7cb800419fdd462a0730f273c7ecc79efcf39bc90f0d72e2d88de3f
+size 2595
