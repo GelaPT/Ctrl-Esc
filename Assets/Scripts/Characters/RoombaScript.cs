8e6b744c177f8d0f6c8895100a7d862b1eebc30e
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 15:34:07 +0000

    No Comments

--- Assets/Scripts/Characters/RoombaScript.cs
+++ Assets/Scripts/Characters/RoombaScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11af2ddc82c9715bc2092187e582d5d963ecbd032e488524330ec23159943cdf
-size 5210
+oid sha256:05ddc2974d566b17524842dbbde15396dbbb38e24a5d10b85056708af2a74fff
+size 5438
--- Assets/Scripts/Props/Door.cs
+++ Assets/Scripts/Props/Door.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:748373fcac7353a04fae8ac67e37ac9664fd0590dcba03bdff1216abc825359a
-size 1389
+oid sha256:746d0f674a61417f2ca918a66a6f8548bc545ec631a00793455ddee6a9bdb523
+size 1562
