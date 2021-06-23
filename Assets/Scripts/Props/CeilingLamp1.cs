19bc844891806db9d8e1f0fcfc2bcb880987f1da
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 17:52:52 +0000

    No Comments

--- Assets/Scripts/Props/CeilingLamp1.cs
+++ Assets/Scripts/Props/CeilingLamp1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12e0552b1aa5fa471ccd20ed1407073f7f7112e8164876e7d45f923d4bec6367
-size 3474
+oid sha256:631dda755ae0657fb2bd10194fc1529b8c585bbfd6c07b344b2225d8a3cde325
+size 3475
