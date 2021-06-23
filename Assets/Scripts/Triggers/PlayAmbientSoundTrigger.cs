0929b2f3ae75ba3d282744c6a9741badc93ca894
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 01:05:22 +0000

    No Comments

--- Assets/Scripts/Triggers/PlayAmbientSoundTrigger.cs
+++ Assets/Scripts/Triggers/PlayAmbientSoundTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d14fd62d6c502e2626a5ac3bf8d2f78f1aededf399490c66d081efde72fd1237
-size 373
+oid sha256:6862e48d4d6f860416ba371e4c61331c52d53845f39e3d2d78c91528accded79
+size 588
