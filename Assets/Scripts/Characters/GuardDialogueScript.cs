86efe3dc8115ecfbc8b8787cb3020fb9d6736dbc
GelaPT <andremgmonteiro01@gmail.com>
2021-06-23 17:25:47 +0000

    No Comments

--- Assets/Scripts/Characters/GuardDialogueScript.cs
+++ Assets/Scripts/Characters/GuardDialogueScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac42a90e192c9d58544425c0a4b0c8be3a1fe690b5e698698869644db65bbb8a
-size 4478
+oid sha256:6183f20249be533a49c5203ad07633fe975a06395ba263e7e1d9a3b64236051d
+size 4586
