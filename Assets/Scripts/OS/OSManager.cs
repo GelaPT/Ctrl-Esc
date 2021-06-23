0c9c482b2a7fb8cdb984d780184d71f9c145510f
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 18:42:25 +0000

    No Comments

--- Assets/Scripts/OS/OSManager.cs
+++ Assets/Scripts/OS/OSManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89ca0e536f38fbd4695128d25ab08c577b618a098d1b250b704ef1997dd96892
-size 11038
+oid sha256:56a041da76551654c063ab7a01eff123ff3ddd1e63819840c7a2fc7075eed319
+size 11135
--- Assets/Scripts/Triggers/AfterFuseBox.cs
+++ Assets/Scripts/Triggers/AfterFuseBox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:962a3d220acee4da86552a72e1e2b890bc1bb1883ca699f4bc55e4d1de7f526b
-size 1143
+oid sha256:8cd7cb2c09b7035507fb26628467e793f1c39f2938b321f9773909365c4f0322
+size 1227
