d5a9c82ba1c8b79debdd4883c1bc8bec201577a3
GelaPT <andremgmonteiro@gmail.com>
2021-06-23 17:38:31 +0000

    No Comments

--- Assets/Scripts/UI/Celphone/Celphone.cs
+++ Assets/Scripts/UI/Celphone/Celphone.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7319770317c26b6bfd71556985b7a692b605db76d60e2b93cbfe985062d9fd90
-size 4596
+oid sha256:9646d7c5d02ca7dfeeb5e730d3d3c2fcc5f836a3bf7a35a11335f6e1b019ad8d
+size 5094
