0245070c95803cd9ea35433147de891100f18b8d
GelaPT <andremgmonteiro01@gmail.com>
2021-06-23 15:39:34 +0000

    No Comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01780e6d4ea9d73613f5b94efded717d189fa961237b9b680146032dcb8342ff
-size 5035
+oid sha256:264f30c5826bde48896132faf55256ce1f420ac51cad462acedda0dfdf1bd91a
+size 4966
