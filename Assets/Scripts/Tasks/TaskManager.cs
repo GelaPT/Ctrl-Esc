7b278dcc0bee9f5a2ee2c3ae966865f05a61257f
GelaPT <andremgmonteiro@gmail.com>
2021-06-23 17:54:52 +0000

    No Comments

--- Assets/Scripts/Tasks/TaskManager.cs
+++ Assets/Scripts/Tasks/TaskManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac84d96e25da723a33a54cb6446df76b8fd94c3ee6307b7c4e0a8ee03e7203a8
-size 7290
+oid sha256:e198ebf7c3b5ab3785ebbdada1e61b832d3afff393065f383d355613875c5eda
+size 7958
