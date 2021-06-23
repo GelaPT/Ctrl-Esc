ad3d6ba43875e10c9e38060d762b68b1e6cdcf56
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-23 18:18:33 +0000

    No Comments

--- Assets/Scripts/Tasks/FuseboxTask.cs
+++ Assets/Scripts/Tasks/FuseboxTask.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec75436453c9a3979da807f14ce5e45202ea0e72a56562cbd085ad6fcd9b3cae
-size 7588
+oid sha256:cf43a27cb69c0fc7ed3deeb88a33a05db70d2b43ad505c6c6e2d25933cd48f66
+size 6684
---
+++ Assets/Scripts/Triggers/MonologueTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41d8d81a72225a77704968ab84a9aa4e2ebdf17ce4c1036ed6a5e77251373042
+size 317
