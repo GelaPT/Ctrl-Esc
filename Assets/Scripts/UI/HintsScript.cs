bca36315341946badbfc2be65b174920fc317983
GelaPT <andremgmonteiro@gmail.com>
2021-06-23 19:26:58 +0000

    No Comments

--- Assets/Scripts/UI/HintsScript.cs
+++ Assets/Scripts/UI/HintsScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0bf5ef05c168e0cbc6d1d2f0d75f1ed662435a40e307eff6e5e94ad6928c222
-size 1569
+oid sha256:05eb71c250c4f4949b0bcaeef5cffa93aedd730aec2c052b987c018882cfb326
+size 1401
