e28ae7c6f20d342cff54089a46740a60aeef591b
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-24 00:33:00 +0000

    No Comments

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6f3cab2bd712bf5cb7a1fc4651b4aa585c4739d9cdac049f52bc0788a814467
+oid sha256:eda7ff40f92745734ee74adc6376cd6b1a51073ff9fa91817a27a7caebab0a23
 size 3896
---
+++ Assets/Scripts/PostProcessingStages.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea8fcd008abdfe090fd72834a172a3e6651210475e722e4ee6f4a1fe8614742e
+size 2287
