f27ce95d74a75c1ebfbf7c502857a8ca69618975
GelaPT <andremgmonteiro@gmail.com>
2021-06-24 10:29:02 +0000

    No Comments

--- Assets/Scripts/OS/OSManager.cs
+++ Assets/Scripts/OS/OSManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97d325d707493c9af9684574c4baad450e9a73291c692ee90a5a65cdad4a40ee
-size 11027
+oid sha256:2726b2d5c8d4bd0d32d392fba7c28e120df54f6e338977052e489c7b0c2c6880
+size 10991
--- Assets/Scripts/OS/OSPrograms/OSWCleaner.cs
+++ Assets/Scripts/OS/OSPrograms/OSWCleaner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d565cf8b323e59433b168ec113734ef99761484be642eb76a2ebda896ef7964
-size 1111
+oid sha256:b8fac996df5e992b6c6f18c3304e07d66b23f2592b37fe1c9b4ad495ebec3a33
+size 1112
