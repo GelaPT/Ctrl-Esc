823ab9ed541d2ed114fcf2b57c854ab84e6f898f
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-24 08:55:15 +0000

    No Comments

--- Assets/Scripts/Characters/GuardDialogueScript.cs
+++ Assets/Scripts/Characters/GuardDialogueScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd6452c2eb25967d2a77a960ce2268199edc5cf5c282b3e3f97b7e1d29cde6d9
-size 4448
+oid sha256:b1e03c67be7e8ed5baeb355d5d3403a86eaa991b2fc8b7fe682269ee47fdc8bb
+size 4716
--- Assets/Scripts/Props/CeilingLamp1.cs
+++ Assets/Scripts/Props/CeilingLamp1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc4fef6cf22612e3e8e02090ba4f7deba4461d2dfd031ccf4f57f4384e464289
-size 3830
+oid sha256:2a3125ce515b627f903071a4216f60e4dd26e4b203792adcb8d07da8951925d4
+size 3949
