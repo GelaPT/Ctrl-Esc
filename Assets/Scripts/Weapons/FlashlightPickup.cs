f6287e9aa5456e0380e121a8c65adb214b05a977
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-24 10:08:55 +0000

    No Comments

--- Assets/Scripts/Weapons/FlashlightPickup.cs
+++ Assets/Scripts/Weapons/FlashlightPickup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abcc26f8c4ab1676be0fe658772a0d9c6b36d9950c56ce820e7cda5c98df76ec
-size 384
+oid sha256:fd55564d2813c7222a5816c8e177c8463a2af11393f86b551be6a6a2869ff2d0
+size 448
