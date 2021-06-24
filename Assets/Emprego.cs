fe90cdc306e67a7dbfd91ac657e250013aa85497
GelaPT <andremgmonteiro@gmail.com>
2021-06-24 18:24:30 +0000

    No Comments

---
+++ Assets/Emprego.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6b28ecf65c29c44c8cbfe1ef2a6e5ec3e94eddec079e403f9f11c71054f7b1f8
+size 130
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8bc2682f87bf3e5a1eff7b43fe0530e2f1cf293f1b9bf31b64cf4b9c7d8c5ac
-size 7394
+oid sha256:d2f4f8460557af2a1e3fde93177760d31d9bdfb559a96d67952b1741f7969a30
+size 7638
