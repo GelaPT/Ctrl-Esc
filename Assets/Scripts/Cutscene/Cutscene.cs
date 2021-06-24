7d2b855e0db574952d0b54f2b18d41eb5c3ed814
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-24 00:39:07 +0000

    No Comments

--- Assets/Scripts/Cutscene/Cutscene.cs
+++ Assets/Scripts/Cutscene/Cutscene.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a61d321936212e107bb9d69ccf9ae9608060157bc1512f90b5054b16a102a70b
+oid sha256:2d6f31f01aaf919c9a2525b9b5654f8d871dc33af652a0d8a5eef943bda37dbc
 size 1162
