806f18e8d17452da1f64285661cbf785221e473d
GelaPT <andremgmonteiro01@gmail.com>
2021-06-24 10:26:14 +0000

    No Comments

--- Assets/Scripts/OS/OSPrograms/OSWCleaner.cs
+++ Assets/Scripts/OS/OSPrograms/OSWCleaner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ad1681ee0bb80421a257100033e4c0584fec137bbbec95c6e8734b6146616b6
-size 723
+oid sha256:1d565cf8b323e59433b168ec113734ef99761484be642eb76a2ebda896ef7964
+size 1111
