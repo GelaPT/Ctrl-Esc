eee88ee6baa55f0e73bb4eef87a1ae232327dafc
GelaPT <andremgmonteiro@gmail.com>
2021-06-24 11:37:40 +0000

    No Comments

--- Assets/Scripts/Characters/DeadMannequinScript.cs
+++ Assets/Scripts/Characters/DeadMannequinScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:035dd27c24c2cd22f73430abaded75ec2aa7c6abb73b2b390ffdfe211926a19c
-size 4393
+oid sha256:d845c52c0e5d2aeaa3b4c4c6327c1934bf19621febe374dd310efd11f63da928
+size 4427
