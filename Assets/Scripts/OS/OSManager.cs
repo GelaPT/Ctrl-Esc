97a8183a4732f2f8f9e9bf06805626fe7aa0f44d
GelaPT <andremgmonteiro01@gmail.com>
2021-06-24 22:02:27 +0000

    No Comments

--- Assets/Scripts/OS/OSManager.cs
+++ Assets/Scripts/OS/OSManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:724561c8c89f0c7a41055d05ccdc4defcf11f4e7d8625fc5177f0edd39adf22c
-size 10945
+oid sha256:fc949f04f51ac6874a2916bd508005d7f7c714da47c34aeead1c5ae7c2394bf0
+size 10915
--- Assets/Scripts/Triggers/AfterFuseBox.cs
+++ Assets/Scripts/Triggers/AfterFuseBox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a532e8989fe689ca38976596407704ebf9141db158a00f43ee4044bd52bdfe6
-size 1827
+oid sha256:172f6d2f2cf33b4c06af66c90374144c4b2b6c9976435ba94ef43551c0fa6e10
+size 1933
