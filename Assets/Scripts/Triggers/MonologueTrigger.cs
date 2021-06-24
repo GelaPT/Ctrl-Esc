132701ee4107b4cac3be638aff777309a0042381
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-24 09:58:23 +0000

    No Comments

--- Assets/Scripts/Triggers/MonologueTrigger.cs
+++ Assets/Scripts/Triggers/MonologueTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:488a0c76ca5cccb8aaeb5600a32bcba22a7884ca6f2928152adc162ee011f8eb
-size 578
+oid sha256:2ad109e5d45156ae5c66420fafe2547746b27380faa270d2b1e91a3608daeab7
+size 616
