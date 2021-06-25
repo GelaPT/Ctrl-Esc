ae391fa4454b69f165c52935dcc5e880af549245
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-25 01:41:34 +0000

    No Comments

--- Assets/Scripts/Characters/VirusDialogueScript.cs
+++ Assets/Scripts/Characters/VirusDialogueScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba3d483bfa8fc81a508b686d05eb31492748c08bd80ac416dafca9f58baaeb9d
-size 4552
+oid sha256:4d4b7180b915818ea8e95783f83be7d903bee841a959df9704da0dfda464375e
+size 4650
