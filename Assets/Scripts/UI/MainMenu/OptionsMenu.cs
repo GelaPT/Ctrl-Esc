e1dd67e733fded013502a7ee79fc99a50ddb0e56
Lucas <lucas_cmattos@hotmail.com.br>
2021-06-25 00:31:16 +0000

    No Comments

--- Assets/Scripts/UI/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/UI/MainMenu/OptionsMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89c54750669e4013144270a322520934e74acbbdf51a5c5e147fadfca8293759
-size 3910
+oid sha256:e71e1fdd149931c2a19f252c68a2d634f08491a5d110b8a4f76d55303f52d3ea
+size 3997
